e433b258e439559f5fd88eb3e1f20446667a66cb
Michael <88445485+mjnyland@users.noreply.github.com>
2024-03-13 07:48:19 -0400

    first commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Michael Nyland on 3/13/24.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
